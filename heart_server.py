28e7e1f92439da74fe741d513ef5861b4cef852b
Matthew Huber <matthew.huber@duke.edu>
2018-11-10 14:09:50 -0500

    added new_patient method to load patient into database

---
+++ heart_server.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route("/api/new_patient", methods = ["POST"])
+def new_patient():
+    patient_data = request.get_json()
+    pat_id = patient_data["patient_id"]
+    email = patient_data["attending_email"]
+    age = patient_data["user_age"]
+    pat_to_add = Patient(patient_id = pat_id, attending_email = email, user_age = age)
+    pat_to_add.save()
+    
+    return 0
