133e9d0a451f3fe9025f3a3a2ca87d091bf81c82
Matthew Huber <matthew.huber@duke.edu>
2018-11-12 14:48:24 -0500

    pep8 corrections

--- db_patient.py
+++ db_patient.py
@@ -1,12 +1,13 @@
 from pymodm import connect
 from pymodm import MongoModel, fields
 
-connect("mongodb://heart-rate-db:GODUKE10@ds159263.mlab.com:59263/bme590heartdata")
+connect(
+    "mongodb://heart-rate-db:GODUKE10@ds159263.mlab.com:59263/bme590heartdata")
+
 
 class Patient(MongoModel):
-    patient_id = fields.IntegerField(primary_key = True)
-    attending_email = fields.EmailField() 
+    patient_id = fields.IntegerField(primary_key=True)
+    attending_email = fields.EmailField()
     user_age = fields.IntegerField()
     heart_rate = fields.ListField()
     h_r_times = fields.ListField()
-
