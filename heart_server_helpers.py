141de635ae8d08a370e8024937f6ca354f203d53
Matthew Huber <matthew.huber@duke.edu>
2018-11-13 16:13:29 -0500

    created function is_tachycardic which checks database for patient, and determines whether that patient is tachycardic or not;

---
+++ heart_server_helpers.py
@@ -0,0 +1,31 @@
+from db_patient import Patient
+
+
+def is_tachycardic(patient_id):
+    for user in Patient.objects.raw({"_id": patient_id})
+        patient = user
+
+    age = patient.user_age
+    heart_rate = patient.herat_rate
+    if age < 1:
+        except 
+    elif age < 3:
+        if heart_rate > 151:
+            return True
+    elif age < 5:
+        if heart_rate > 137:
+            return True
+    elif age < 8:
+        if heart_rate > 133:
+            return True
+    elif age < 12:
+        if heart_rate > 130:
+            return True
+    elif age < 15:
+        if heart_rate > 119:
+            return True
+    else:
+        if heart_rate > 100:
+            return True
+    
+    return False
