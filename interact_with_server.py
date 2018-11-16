faed8998a577a097e2cb3dc969fe298b5eb15722
Matthew Huber <matthew.huber@duke.edu>
2018-11-16 10:26:16 -0500

    added docstrings describing functions that interact with server

--- interact_with_server.py
+++ interact_with_server.py
@@ -1,17 +1,26 @@
 import requests
-import json
 
 
-def main():
+def add_patient():
+    """
+    Add a new patient to the database
+
+    :return: None
+    """
     r = requests.post("http://127.0.0.1:5000/api/new_patient",
-                      json={"patient_id": 8,
+                      json={"patient_id": 9,
                             "attending_email": "mth37@duke.edu",
                             "user_age": 50})
     post_result = r.json()
     print(post_result)
 
 
-def main2():
+def add_hr():
+    """
+    Add a new heart rate to the database for a given patient
+
+    :return: None
+    """
     r = requests.post("http://127.0.0.1:5000/api/heart_rate",
                       json={"patient_id": 8,
                             "heart_rate": 200})
@@ -19,19 +28,34 @@ def main2():
     print(post_result)
 
 
-def main3():
+def get_last_hr():
+    """
+    find the last heart rate stored in database for a patient
+
+    :return: None
+    """
     r = requests.get("http://127.0.0.1:5000/api/heart_rate/10")
     result = r.json()
     print(result)
 
 
-def main4():
+def get_avg_hr():
+    """
+    Find average heart rate for a patient through all time
+
+    :return: None
+    """
     r = requests.get("http://127.0.0.1:5000/api/heart_rate/average/10")
     result = r.json()
     print(result)
 
 
-def main5():
+def get_avg_hr_since():
+    """
+    Find average heart for a patient since given time
+
+    :return: None
+    """
     r = requests.post("http://127.0.0.1:5000/api/heart_rate/interval_average",
                       json={"patient_id": 8,
                             "heart_rate_average_since":
@@ -41,4 +65,4 @@ def main5():
 
 
 if __name__ == "__main__":
-    main5()
+    add_patient()
